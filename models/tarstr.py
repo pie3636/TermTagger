34b575c7f4d9a7d0c3d1c784cf84409902e5202c
thebugcreator <thebugcreator@github.com>
2022-11-30 15:47:43 +0100

    Removed the useless sentence calls

--- models/tarstr.py
+++ models/tarstr.py
@@ -3,12 +3,11 @@ from flair.data import Sentence
 
 tag_type = "ner"
 
-def create_fsentence(_input) -> Sentence:
+def create_fsentence(_input, verbose=False) -> Sentence:
     tokens, tags = _input
     sentence = Sentence(" ".join(tokens))
-    print(sentence)
     if len(sentence) != len(tokens):
-        print("Trouble: ", sentence)
+        if verbose: print("Trouble: ", sentence)
         return sentence
     for i, token in enumerate(tokens):
         if tags[i] == 2:
@@ -21,6 +20,6 @@ def create_fsentence(_input) -> Sentence:
                     offset += 1 
                 else:
                     break
-            print(onset, offset)
+            if verbose: print(onset, offset)
             sentence[onset:offset].add_label(tag_type, value="term")
     return sentence
