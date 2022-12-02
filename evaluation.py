9d623a5754092aeffae252f8638291c91a868acf
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2022-12-02 11:49:23 +0100

    Give spans a type

--- evaluation.py
+++ evaluation.py
@@ -1,10 +1,10 @@
 from flair.data import Sentence
+from models.tarstr import flair_tag_type
 
-flair_tag_type = "ner"
 
 def convert_span_into_iob(_in:Sentence) -> list:
     tokens = [tok for tok in _in]
-    spans = _in.get_spans()
+    spans = _in.get_spans(flair_tag_type)
     iob = ["O"]*len(tokens)
     for span in spans:
         for i, token in enumerate(span):
