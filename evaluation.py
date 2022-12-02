081ea3faa46d262eba2adf9008baf4c649e9fc25
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2022-12-02 11:45:14 +0100

    Fixing a minor mistake from the past

--- evaluation.py
+++ evaluation.py
@@ -3,7 +3,7 @@ from flair.data import Sentence
 flair_tag_type = "ner"
 
 def convert_span_into_iob(_in:Sentence) -> list:
-    tokens = [tok for tok in _in)]
+    tokens = [tok for tok in _in]
     spans = _in.get_spans()
     iob = ["O"]*len(tokens)
     for span in spans:
