e45a9cb7f894ba8859f0f6ff8601794f13e32a9a
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2022-12-02 11:42:33 +0100

    My first contribution (no it's not)

---
+++ evaluation.py
@@ -0,0 +1,16 @@
+from flair.data import Sentence
+
+flair_tag_type = "ner"
+
+def convert_span_into_iob(_in:Sentence) -> list:
+    tokens = [tok for tok in _in)]
+    spans = _in.get_spans()
+    iob = ["O"]*len(tokens)
+    for span in spans:
+        for i, token in enumerate(span):
+            if i == 0:
+                iob[token.idx] = "B"
+            else:
+                iob[token.idx] = "I"
+    return iob
+
