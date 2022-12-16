a1a19fba55c45733aadeff28c121793fee0aeb95
pie3636 <maxime.meloux@protonmail.com>
2022-12-16 21:26:54 +0100

    Improve sandbox a little bit

--- sandbox.py
+++ sandbox.py
@@ -9,8 +9,10 @@ flair.device = torch.device('cpu')
 print('Loading TARSTagger, this may take a minute...')
 
 tagger = TARSTagger.load('output/final-model.pt')
+print()
 
 print('Interactive mode: Type a sentence. The tagged version of the sentence will then be outputed. Stop at any time using Ctrl-C')
+print()
 
 while True:
     sent = Sentence(input('Input: '))
