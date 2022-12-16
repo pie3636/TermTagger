a807947493ece4db3a4d16dc971ea2ad0c370cef
pie3636 <maxime.meloux@protonmail.com>
2022-12-16 21:19:25 +0100

    Add sandbox

---
+++ sandbox.py
@@ -0,0 +1,19 @@
+import flair
+import torch
+
+from flair.data import Sentence
+from flair.models import TARSTagger
+
+flair.device = torch.device('cpu')
+
+print('Loading TARSTagger, this may take a minute...')
+
+tagger = TARSTagger.load('output/final-model.pt')
+
+print('Interactive mode: Type a sentence. The tagged version of the sentence will then be outputed. Stop at any time using Ctrl-C')
+
+while True:
+    sent = Sentence(input('Input: '))
+    tagger.predict(sent)
+    print('Prediction: ', sent)
+    print()
