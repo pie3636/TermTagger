0b270ff8240e5a84b7d9d5bec09815883459c6cc
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2022-12-16 16:13:12 +0100

    Added the function to get the spans

--- evaluation.py
+++ evaluation.py
@@ -14,3 +14,20 @@ def convert_span_into_iob(_in:Sentence) -> list:
                 iob[token.idx] = "I"
     return iob
 
+
+def get_spans_from_iobs(tags:list, verbose=False):
+    spans = []
+    for i, tag in enumerate(tags):
+        if tag == 2:
+            continue
+        if tag == 0:
+            onset = i
+            offset = i+1
+            for t in tags[i+1:]:
+                if t == 1:
+                    offset += 1
+                else:
+                    break
+            if verbose: print(onset, offset)
+            spans.append([onset,offset])
+    return spans
